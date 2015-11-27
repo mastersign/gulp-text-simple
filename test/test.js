ad8bc3673c0310b9be1a75abf3e1a6a95224cb46
Tobias Kiertscher <dev@mastersign.de>
2015-11-27 10:38:40 +0100

    added test environment

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/* globals require, Buffer, describe, it */
+
+var assert = require('assert');
+var File = require('vinyl');
+var tf = require('../src/index.js');
+
+describe('gulp-simple-text', function () {
+
+
+
+});
